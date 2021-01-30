7c6c0cfa79546c59e41818dcc2de32afb5c5ae88
Tyouxik <paulinemarie.didier@gmail.com>
2021-01-30 14:56:34 +0100

    place stylemap in its own file

---
+++ public/stylemap.js
@@ -0,0 +1,23 @@
+// Custom overrides for each style
+const styleMap = {
+  CODE: {
+    backgroundColor: "rgba(0, 0, 0, 0.05)",
+    fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
+    fontSize: 16,
+    padding: 4,
+  },
+  RED: {
+    color: "red",
+  },
+  BLACK: {
+    color: "black",
+  },
+  BLUE: {
+    color: "blue",
+  },
+  GREEN: {
+    color: "green",
+  },
+};
+
+export { styleMap };
