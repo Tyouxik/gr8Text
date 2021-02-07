84dab8e4a9dea9c55c82ead2bf38b8b05f012860
Tyouxik <paulinemarie.didier@gmail.com>
2021-02-07 11:12:08 +0100

    add default content when lesson is added

--- pages/api/course/[courseId]/addlesson.js
+++ pages/api/course/[courseId]/addlesson.js
@@ -2,6 +2,63 @@ import db from "../../../../utils/db/index";
 import nc from "next-connect";
 import { collectIdsAndData } from "../../../../utils/utilities";
 
+const startContent = {
+  blocks: [
+    {
+      key: "16d0k",
+      text: "",
+      type: "unstyled",
+      depth: 0,
+      inlineStyleRanges: [],
+      entityRanges: [],
+      data: {},
+    },
+    {
+      key: "98peq",
+      text: "Wake up at a regular time ",
+      type: "unstyled",
+      depth: 0,
+      inlineStyleRanges: [],
+      entityRanges: [],
+      data: {},
+    },
+    {
+      key: "6lbia",
+      text: "",
+      type: "unstyled",
+      depth: 0,
+      inlineStyleRanges: [],
+      entityRanges: [],
+      data: {},
+    },
+    {
+      key: "49tel",
+      text: "Sleep is essential, you should make it a priority.",
+      type: "unstyled",
+      depth: 0,
+      inlineStyleRanges: [
+        {
+          offset: 0,
+          length: 20,
+          style: "BOLD",
+        },
+      ],
+      entityRanges: [],
+      data: {},
+    },
+    {
+      key: "fe2gn",
+      text: "",
+      type: "unstyled",
+      depth: 0,
+      inlineStyleRanges: [],
+      entityRanges: [],
+      data: {},
+    },
+  ],
+  entityMap: {},
+};
+
 const handler = nc().post(async (req, res) => {
   try {
     const { courseId } = req.query;
@@ -10,8 +67,11 @@ const handler = nc().post(async (req, res) => {
       .collection("courses")
       .doc(courseId)
       .collection("lessons");
-    const docRef = await lessonsRef.add({ title: newLessonTitle });
-    console.log({ docRef });
+
+    const docRef = await lessonsRef.add({
+      title: newLessonTitle,
+      content: startContent,
+    });
     const doc = await docRef.get();
     const newLesson = collectIdsAndData(doc);
     res.json(newLesson);
