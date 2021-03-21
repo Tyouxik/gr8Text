d08630805960be9e06d47ef01b718ae1396816aa
Tyouxik <paulinemarie.didier@gmail.com>
2021-03-21 17:50:58 +0100

    adds a course to database

--- pages/courses.js
+++ pages/courses.js
@@ -1,99 +1,47 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "../styles/courses.module.css";
-
 import CourseCard from "../public/Components/CourseCard";
+import AddCourse from "../public/Components/AddCourse";
 
 export default function courses() {
-  const [courses, setCourses] = useState([]);
-  const [newCourseTitle, setNewCourseTitle] = useState("");
-  const [newCourseCategory, setNewCourseCategory] = useState("Online Training");
-  const [newCoursePrice, setNewCoursePrice] = useState("Free");
+  const [courses, setCourses] = useState([{}]);
   const [showAddCourse, setShowAddCourse] = useState(false);
 
   useEffect(async () => {
-    const snapshot = await axios.get("/api/courses");
-    const courses = snapshot.data;
-    setCourses(courses);
+    const courses = await axios.get(`/api/courses`);
+    setCourses(courses.data);
   }, []);
 
-  const coursesDisplay = courses ? (
-    courses.map((course) => {
-      return <CourseCard course={course} key={course.id} />;
-    })
-  ) : (
-    <></>
-  );
+  const coursesDisplay =
+    courses.length !== 0 ? (
+      courses.map((course) => {
+        return <CourseCard course={course} />;
+      })
+    ) : (
+      <></>
+    );
 
-  const addCourse = async (e) => {
-    e.preventDefault();
-    const newCourseRef = await axios.post("/api/courses", {
-      newCourseTitle,
-      newCourseCategory,
-      newCoursePrice,
-    });
-    const newCourse = newCourseRef.data;
-    setCourses([newCourse, ...courses]);
-    setShowAddCourse(false);
-    setNewCourseTitle("");
-    setNewCourseCategory("Online Training");
-    setNewCoursePrice("Free");
-  };
-
-  const handleChange = (e) => {
-    const { value, name } = e.target;
-    if (name === "title") {
-      setNewCourseTitle(value);
-    }
-    if (name === "category") {
-      setNewCourseCategory(value);
-    }
-    if (name === "price") {
-      setNewCoursePrice(value);
-    }
-  };
-  const toggleNewCourse = () => {
+  const toggleAddCourse = () => {
     setShowAddCourse(!showAddCourse);
   };
+
   return (
     <>
-      <div onClick={toggleNewCourse} className={styles.add_course_btn}>
-        New Course
-      </div>
-
       {showAddCourse && (
-        <>
-          <div
-            className={styles.add_course_blurred_background}
-            onClick={toggleNewCourse}
-          ></div>
-          <div className={`${styles.add_course}`}>
-            <input
-              type="text"
-              name="title"
-              value={newCourseTitle}
-              onChange={handleChange}
-              placeholder="Course Title"
-            />
-            <select name="category" id="category" onChange={handleChange}>
-              <option value="Online Training">Online Training</option>
-              <option value="Offline Training">Offline Training</option>
-              <option value="Coaching">Coaching</option>
-              <option value="Consultation">Consultation</option>
-            </select>
-
-            <select name="price" id="price" onChange={handleChange}>
-              <option value="Free">Free</option>
-              <option value="Premium">Premium</option>
-            </select>
-            <button className={styles.add_course_btn} onClick={addCourse}>
-              Add a course
-            </button>
-          </div>
-        </>
+        <AddCourse
+          toggleAddCourse={toggleAddCourse}
+          courses={courses}
+          setCourses={setCourses}
+        />
       )}
 
-      <div className={styles.course_cards}>{coursesDisplay}</div>
+      <div className={styles.course_cards}>
+        <div onClick={toggleAddCourse} className={styles.add_course_btn}>
+          New Course
+        </div>
+        {coursesDisplay}
+      </div>
     </>
   );
 }
---
+++ public/Components/AddCourse.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import axios from "axios";
+import styles from "../../styles/courses.module.css";
+
+export default function AddCourse({ courses, setCourses, toggleAddCourse }) {
+  const [newCourseTitle, setNewCourseTitle] = useState("");
+  const [newCourseCategory, setNewCourseCategory] = useState("Online Training");
+  const [newCoursePrice, setNewCoursePrice] = useState("Free");
+  const [currentUser, setCurrentUser] = useState("Anonymous");
+
+  const addCourse = async (e) => {
+    e.preventDefault();
+    const newCourseRef = await axios.post("/api/courses", {
+      newCourseTitle,
+      newCourseCategory,
+      newCoursePrice,
+      currentUser,
+    });
+    const newCourse = newCourseRef.data;
+    setCourses([newCourse, ...courses]);
+    setShowAddCourse(false);
+    setNewCourseTitle("");
+    setNewCourseCategory("Online Training");
+    setNewCoursePrice("Free");
+  };
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    if (name === "title") {
+      setNewCourseTitle(value);
+    }
+    if (name === "category") {
+      setNewCourseCategory(value);
+    }
+    if (name === "price") {
+      setNewCoursePrice(value);
+    }
+  };
+
+  return (
+    <>
+      <div
+        className={styles.add_course_blurred_background}
+        onClick={toggleAddCourse}
+      ></div>
+      <div className={`${styles.add_course}`}>
+        <input
+          type="text"
+          name="title"
+          value={newCourseTitle}
+          onChange={handleChange}
+          placeholder="Course Title"
+        />
+        <select name="category" id="category" onChange={handleChange}>
+          <option value="Online Training">Online Training</option>
+          <option value="Offline Training">Offline Training</option>
+          <option value="Coaching">Coaching</option>
+          <option value="Consultation">Consultation</option>
+        </select>
+
+        <select name="price" id="price" onChange={handleChange}>
+          <option value="Free">Free</option>
+          <option value="Premium">Premium</option>
+        </select>
+        <button className={styles.add_course_btn} onClick={addCourse}>
+          Add a course
+        </button>
+      </div>
+    </>
+  );
+}
