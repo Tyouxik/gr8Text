8ff8058f15d3d3accee1c90267f1018879c755a0
Tyouxik <paulinemarie.didier@gmail.com>
2021-04-14 14:29:16 +0200

    create fetcher to transfer token to backend

--- utils/utilities.js
+++ utils/utilities.js
@@ -1,1 +1,11 @@
 export const collectIdsAndData = (doc) => ({ id: doc.id, ...doc.data() });
+
+export const fetcher = async (url, token) => {
+  const res = await fetch(url, {
+    method: "GET",
+    headers: new Headers({ "Content-Type": "application/json", token }),
+    credentials: "same-origin",
+  });
+
+  return res.json();
+};
