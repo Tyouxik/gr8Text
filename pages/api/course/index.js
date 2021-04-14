14af90b3286226aeb68c8f344f03cc106bd3d761
Tyouxik <paulinemarie.didier@gmail.com>
2021-04-14 14:25:41 +0200

    add dashboard page and courses created by a logged user

---
+++ pages/api/course/index.js
@@ -0,0 +1,44 @@
+import { db, auth } from "../../../utils/db/index";
+import nc from "next-connect";
+import { collectIdsAndData } from "../../../utils/utilities";
+
+const handler = nc()
+  .get(async (req, res) => {
+    console.log("I am called");
+    const { token } = req.headers;
+    const { uid } = await auth.verifyIdToken(token);
+    const coursesRef = await db
+      .collection("courses")
+      .where("creator", "=", uid)
+      .get();
+    const courses = coursesRef.docs.map(collectIdsAndData);
+    res.json(courses);
+  })
+  .post(async (req, res) => {
+    try {
+      const {
+        newCourseTitle,
+        newCourseCategory,
+        newCoursePrice,
+        token,
+      } = req.body;
+
+      const { uid } = await auth.verifyIdToken(token);
+
+      const course = {
+        title: newCourseTitle,
+        category: newCourseCategory,
+        price: newCoursePrice,
+        access: "private",
+        creator: uid,
+      };
+
+      const docRef = await db.collection("courses").add(course);
+      const doc = await docRef.get();
+      const newCourse = collectIdsAndData(doc);
+      res.json(newCourse);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+export default handler;
---
+++ pages/dashboard.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styles from "../styles/courses.module.scss";
+import CourseCard from "../public/Components/CourseCard";
+import AddCourse from "../public/Components/AddCourse";
+import Navbar from "../public/Components/Navbar";
+import { useAuth } from "../utils/auth-context";
+import { fetcher } from "../utils/utilities";
+import { useRouter } from "next/router";
+
+export default function dashboard() {
+  const router = useRouter();
+
+  const [courses, setCourses] = useState([{}]);
+  const [showAddCourse, setShowAddCourse] = useState(false);
+  const { auth, loading } = useAuth();
+
+  useEffect(() => {
+    // If auth is null and we are no longer loading
+    if (!auth && !loading) {
+      // redirect to index
+      router.push("/signup");
+    }
+  }, [auth, loading]);
+
+  useEffect(async () => {
+    if (auth) {
+      const courses = await fetcher(`/api/course`, auth.token);
+      console.log(courses);
+      setCourses(courses);
+    }
+  }, [auth]);
+
+  const coursesDisplay =
+    courses.length !== 0 ? (
+      courses.map((course) => {
+        return <CourseCard course={course} />;
+      })
+    ) : (
+      <></>
+    );
+
+  const toggleAddCourse = () => {
+    setShowAddCourse(!showAddCourse);
+  };
+
+  return (
+    <>
+      {showAddCourse && (
+        <AddCourse
+          toggleAddCourse={toggleAddCourse}
+          courses={courses}
+          setCourses={setCourses}
+          setShowAddCourse={setShowAddCourse}
+        />
+      )}
+      <Navbar />
+      <div className={styles.course_cards}>
+        <div
+          onClick={toggleAddCourse}
+          className={`${styles.card} ${styles.add_course_btn}`}
+        >
+          <p>New Course</p>
+        </div>
+        {coursesDisplay}
+      </div>
+    </>
+  );
+}
