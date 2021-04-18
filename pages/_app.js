c5933abc03f85d3068d6108177d088b92dd559f1
Tyouxik <paulinemarie.didier@gmail.com>
2021-04-18 11:50:32 +0200

    add navbar to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { AppProps } from "next/app";
 import { AuthProvider } from "../utils/auth-context";
 import "../styles/globals.scss";
+import Navbar from "../public/Components/Navbar";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Navbar />
       <Component {...pageProps} />
     </AuthProvider>
   );
