16945d3d33eef35e79437a47d95f60ee8989e50b
Tyouxik <paulinemarie.didier@gmail.com>
2021-04-19 22:15:38 +0200

    style courseLessonPlan

--- public/Components/CourseLessonPlan.js
+++ public/Components/CourseLessonPlan.js
@@ -1,74 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../styles/course.module.scss";
 import { useCourse } from "../../utils/course-context";
+import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
 
 export default function CourseLessonPlan() {
   const {
     lessons,
     activeLesson,
-    addLesson,
     deleteLesson,
     toggleActiveLesson,
-    newLessonTitle,
-    setNewLessonTitle,
   } = useCourse();
+  const [showContent, setShowContent] = useState(false);
+  if (!lessons) return <></>;
 
-  console.log({
-    lessons,
-    activeLesson,
-    addLesson,
-    deleteLesson,
-    toggleActiveLesson,
-    newLessonTitle,
-    setNewLessonTitle,
-  });
-  if (!lessons)
+  const lessonPlan = () => {
+    const lessonList = lessons.map((lesson) => {
+      let style;
+      if (activeLesson && lesson.id === activeLesson.id) {
+        style = `${styles.lesson} ${styles.activeLesson}`;
+      } else {
+        style = `${styles.lesson}`;
+      }
+      return (
+        <li className={style} onClick={() => toggleActiveLesson(lesson.id)}>
+          <h3>{lesson.title}</h3>
+          <AiOutlineMinusCircle onClick={() => deleteLesson(lesson.id)} />
+        </li>
+      );
+    });
+
     return (
-      <>
-        <h1>No lessons</h1>
-      </>
+      <ul>
+        {lessonList}
+        <AddLesson />
+      </ul>
     );
+  };
 
-  const lessonPlan = lessons.map((lesson) => {
-    let style;
-    if (activeLesson && lesson.id === activeLesson.id) {
-      style = `${styles.lesson} ${styles.activeLesson}`;
-    } else {
-      style = `${styles.lesson}`;
-    }
-    return (
-      <div className={style} onClick={() => toggleActiveLesson(lesson.id)}>
-        <h3>{lesson.title}</h3>
-        <button onClick={() => deleteLesson(lesson.id)}>-</button>
+  const toggleContent = () => {
+    console.log("I am triggered", showContent);
+    setShowContent(!showContent);
+  };
+
+  return (
+    <>
+      <div className={styles.coursePlan}>
+        <button className={styles.lessonTitle} onClick={toggleContent}>
+          <h2>Lesson plan</h2>
+        </button>
+        {showContent && <div className={styles.lessonList}>{lessonPlan()}</div>}
       </div>
-    );
-  });
+    </>
+  );
+}
+
+function AddLesson() {
+  const { newLessonTitle, setNewLessonTitle, addLesson } = useCourse();
+
   const handleChange = (e) => {
     const { value } = e.target;
     setNewLessonTitle(value);
   };
 
   return (
-    <>
-      <div className={styles.coursePlan}>
-        <h2 className={styles.lessonTitle}>Lesson plan</h2>
-        {lessonPlan}
-        <div className={styles.add_lesson}>
-          <input
-            type="text"
-            value={newLessonTitle}
-            onChange={handleChange}
-            placeholder="Add a New lesson"
-          />
-          <button
-            disabled={newLessonTitle === ""}
-            className={styles.add_btn}
-            onClick={addLesson}
-          >
-            +
-          </button>
-        </div>
-      </div>
-    </>
+    <li className={styles.add_lesson}>
+      <input
+        type="text"
+        value={newLessonTitle}
+        onChange={handleChange}
+        placeholder="Add a New lesson"
+      />
+      <button
+        disabled={newLessonTitle === ""}
+        className={styles.add_btn}
+        onClick={addLesson}
+      >
+        <AiOutlinePlusCircle />
+      </button>
+    </li>
   );
 }
