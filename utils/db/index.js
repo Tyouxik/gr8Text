21a9145cac2028a4bce258f89b709842bd939135
Tyouxik <paulinemarie.didier@gmail.com>
2021-05-03 22:21:49 +0200

    fix deployment

--- utils/db/index.js
+++ utils/db/index.js
@@ -4,7 +4,7 @@ if (!admin.apps.length) {
   try {
     admin.initializeApp({
       credential: admin.credential.cert({
-        project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        project_id: process.env.FIREBASE_PROJECT_ID,
         private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
         client_email: process.env.FIREBASE_CLIENT_EMAIL,
       }),
