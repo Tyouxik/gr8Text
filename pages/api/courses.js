d8673231a867fd7c56f0b68d6e6e53606dbe632f
Tyouxik <paulinemarie.didier@gmail.com>
2021-05-04 19:29:54 +0200

    fix deployment

--- pages/api/courses.js
+++ pages/api/courses.js
@@ -3,17 +3,21 @@ import nc from "next-connect";
 import { collectIdsAndData } from "../../utils/utilities";
 
 const handler = nc().get(async (req, res) => {
-  const snapshot = await db
-    .collection("courses")
-    .where("access", "==", "public")
-    .get();
+  try {
+    const snapshot = await db
+      .collection("courses")
+      .where("access", "==", "public")
+      .get();
 
-  if (snapshot.empty) {
-    console.log("No matching documents.");
-    return;
+    if (snapshot.empty) {
+      console.log("No matching documents.");
+      return;
+    }
+    const courses = snapshot.docs.map(collectIdsAndData);
+    res.status(200).json({ courses });
+  } catch (err) {
+    console.log(err);
   }
-  const courses = snapshot.docs.map(collectIdsAndData);
-  res.status(200).json({ courses });
 });
 
 export default handler;
